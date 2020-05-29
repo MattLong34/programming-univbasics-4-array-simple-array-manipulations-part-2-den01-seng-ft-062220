b1ccc7427e5307770c9a94e1977128aecdaecd77
Matt Long <mdlong34@gmail.com>
2020-05-29 16:14:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,9 @@
-def using_concat(my_favorite_things, all_my_favs)
-  my_favorite_things.concat(all_my_favs)
+# def using_concat(my_favorite_things, all_my_favs)
+#   my_favorite_things.concat(all_my_favs)
+# end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
 end
 
 def using_insert(array, element)
