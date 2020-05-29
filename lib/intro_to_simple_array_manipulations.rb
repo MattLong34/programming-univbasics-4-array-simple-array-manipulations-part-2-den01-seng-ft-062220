1d6244f7e6738cd394651e48f6352e55fc96012f
Matt Long <mdlong34@gmail.com>
2020-05-29 16:15:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,3 @@
-# def using_concat(my_favorite_things, all_my_favs)
-#   my_favorite_things.concat(all_my_favs)
-# end
-
 def using_concat(array1, array2)
   array1.concat(array2)
 end
@@ -9,3 +5,5 @@ end
 def using_insert(array, element)
   array.insert(4, element)
 end
+
+
